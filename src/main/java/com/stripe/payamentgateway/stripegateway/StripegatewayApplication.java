a7a8fe996222970c9f7470512d70b191f8d24792
nabilAesys <nabil.ezzaki@aesys.tech>
2024-11-20 10:48:40 +0100

    init

---
+++ src/main/java/com/stripe/payamentgateway/stripegateway/StripegatewayApplication.java
@@ -0,0 +1,13 @@
+package com.stripe.payamentgateway.stripegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripegatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripegatewayApplication.class, args);
+	}
+
+}
